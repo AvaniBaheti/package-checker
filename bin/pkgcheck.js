76932d3eec102713450e324f99db80dbcc656c8a
Avani Baheti <avani@digitalgreen.org>
2025-07-24 21:06:55 +0530

    feat: add license checking for dependencies with risk warning for nodejs

--- bin/pkgcheck.js
+++ bin/pkgcheck.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 import { parsePackageJson } from '../lib/parser.js';
+import { checkLicenses } from '../lib/license-checker.js';
 
 const args = process.argv.slice(2); 
 const filePath = args[0] || './package.json'; 
 const deps = parsePackageJson(filePath);
+await checkLicenses(deps);
